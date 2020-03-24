d37fe6e0019a5ffee424caed2fe33accc6245886
iqramqra <masrooriqra@gmail.com>
2020-03-24 12:29:33 -0400

    updated

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,9 +1,8 @@
 class WeathersController < ApplicationController
-    include HTTParty
-    # base_uri 'Insert the api link here'
+    # include HTTParty
+    # base_uri 'api.openweathermap.org/data/2.5/forecast?zip=11230,&appid=a6cb209a99baafd785ecd07da0491103'
     # api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={your api key}
-    def index
-
-    end
-
+    # def index
+        # response = HTTParty.get('api.openweathermap.org/data/2.5/forecast?zip=11230,&appid=a6cb209a99baafd785ecd07da0491103')
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :outfits
+  resources :outfits, only: [:index]
   resources :weathers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
